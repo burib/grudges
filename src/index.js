67328b62e99627dd9d6fc4f297524ee094c7fe0b
Balázs Buri <info@burib.net>
2018-06-20 12:13:57 +0200

    connect aws with the client app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
 
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+
+Amplify.configure(awsConfig);
+
 ReactDOM.render(<Application />, document.getElementById('root'));
